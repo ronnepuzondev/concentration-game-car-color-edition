fd6c57177a512cdbaf017d0d71798840e1e07877
rmap88 <ronnepuzondev@gmail.com>
2022-10-17 11:53:20 -0700

    add eventlistener to each image and track data id

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
-console.log('say hello')
+console.log('Howdy folks')
+
+ /*----- constants -----*/
+
+const images = document.querySelectorAll("[data-index]")
+
+
+  /*----- state variables -----*/
+
+let isFirstCardClicked = false
+let firstCardClicked 
+
+
+
+
+  /*----- cached elements  -----*/
+
+
+  /*----- event listeners -----*/
+  images.forEach(function(image) {
+    image.addEventListener("click", function() {
+        console.log(image.dataset)
+    })
+})
+ 
+
+
+  /*----- functions -----*/
+// function below will add an event listener to each image, and give ability to retrieve data-index assigned to it
+
 
