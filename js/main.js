b7fd33671ae82a28eea745711133d76beb8d4095
rmap88 <ronnepuzondev@gmail.com>
2022-10-17 16:29:38 -0700

    add array tracker

--- js/main.js
+++ js/main.js
@@ -1,31 +1,65 @@
-console.log('Howdy folks')
-
  /*----- constants -----*/
 
-const images = document.querySelectorAll("[data-index]")
+const images = document.querySelectorAll(".front")
+
+
+// images.addEventListener("click", function() {
+//     let data = parseInt(front.dataset.index)
+//     console.log(front.dataset.index)
+// })
 
 
   /*----- state variables -----*/
 
-let isFirstCardClicked = false
-let firstCardClicked 
-
-
-
+let isFirstCardClicked = -1
+let firstCard
+let selectedImages = [0]
 
   /*----- cached elements  -----*/
-
+// const onClick = function (event) {
+//     let isFirstCardClicked = true;
+//     let convert = front.dataset.index
+//     console.log(convert)
+// }
 
   /*----- event listeners -----*/
-  images.forEach(function(image) {
+
+// images.forEach(function(image) {
+//     image.addEventListener("click", onClick)
+// })
+
+const eventListener =  images.forEach(function(image) {
     image.addEventListener("click", function() {
-        console.log(image.dataset)
-    })
+        isFirstCardClicked *= -1
+        let data = parseInt(image.dataset.index)
+        selectedImages.push(data)
+        console.log(selectedImages)
+        console.log(isFirstCardClicked)
+        if (selectedImages[1] === selectedImages[2]) {
+            console.log('winner!');
+            selectedImages = []
+            console.log(selectedImages)
+        } else {
+            selectedImages = []
+        }
+        })
 })
- 
+
+// function eventListenerAdd() {
+//     images.forEach(function(image){
+//         image.addEventListener("click", function(){
+//             if (selectedImages[1] === selectedImages[2]) {
+//                 console.log('winner!');
+//                 selectedImages = []
+//                 console.log(selectedImages)
+//             } else {
+//                 selectedImages = []
+//         }
+//     })
+// })
+// }
 
 
-  /*----- functions -----*/
-// function below will add an event listener to each image, and give ability to retrieve data-index assigned to it
 
-
+    
+    /*----- functions -----*/
