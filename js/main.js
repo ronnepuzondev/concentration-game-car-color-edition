48bf57d1b8b97ab01d8411b69e5c58fd93ac0677
rmap88 <ronnepuzondev@gmail.com>
2022-10-17 19:42:25 -0700

    add js for game complete

--- js/main.js
+++ js/main.js
@@ -1,64 +1,55 @@
- /*----- constants -----*/
+/*----- constants -----*/
 
- const images = document.querySelectorAll(".front")
+const images = document.querySelectorAll(".front");
+
 
 // images.addEventListener("click", function() {
 //     let data = parseInt(front.dataset.index)
 //     console.log(front.dataset.index)
 // })
 
+/*----- state variables -----*/
 
-  /*----- state variables -----*/
+let selectedImages = [0];
+let playerScore = 0;
 
-  let selectedImages = [0]
-  let playerScore = []
-
-
-
-  /*----- cached elements  -----*/
+/*----- cached elements  -----*/
 // const onClick = function (event) {
 //     let isFirstCardClicked = true;
 //     let convert = front.dataset.index
 //     console.log(convert)
 // }
 
-  /*----- event listeners -----*/
-
-
+/*----- event listeners -----*/
 
 // need help with "else" condition
- 
-  images.forEach(function(image) {
-    image.addEventListener("click", function() {
-        let data = parseInt(image.dataset.index)
-        selectedImages.push(data)
-        console.log(selectedImages)
-        if (selectedImages[1] === selectedImages[2]) {
-            console.log('winner!');
-            selectedImages = [0]
-            console.log(selectedImages)
-            playerScore.push(1)
-            console.log(playerScore)
-        } else {
-          if (selectedImages.length === 3) {
-            console.log('wrong')
-            selectedImages = [0]
-          }
-        }
-        })
-        })
 
-
-
+images.forEach(function (image) {
+  image.addEventListener("click", function () {
+    let data = parseInt(image.dataset.index);
+    selectedImages.push(data);
+    console.log(selectedImages);
+    if (selectedImages[1] === selectedImages[2]) {
+      console.log("winner!");
+      selectedImages = [0];
+      console.log(selectedImages);
+      playerScore += 1;
+    } else {
+      if (selectedImages.length === 3) {
+        console.log("wrong");
+        selectedImages = [0];
+      }
+    }
+    if (playerScore === 6) {
+      console.log("YOU WIN");
+    }
+  });
+});
 
 // find a way to flip the images - transform / translate
 
 // when all the images are matched correctly function  - iterate all images and see if theres a specific class
 
+//
 
-// 
-
-    /*----- functions -----*/
-
-  
-
+/*----- functions -----*/
