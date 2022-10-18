3e064c8735b34f351fd7fcc93bdda1825b7461fa
rmap88 <ronnepuzondev@gmail.com>
2022-10-18 15:32:31 -0700

    add way to remove matched images

--- js/main.js
+++ js/main.js
@@ -1,32 +1,27 @@
 /*----- constants -----*/
 
 const images = document.querySelectorAll(".front");
-const scoreTracker = document.querySelector(".score")
+const scoreTracker = document.querySelector(".score");
+const btn = document.querySelector("#playButton");
+const pictures = document.querySelectorAll(".one");
 
-
-// images.addEventListener("click", function() {
-//     let data = parseInt(front.dataset.index)
-//     console.log(front.dataset.index)
-// })
+// if you want to use array of images somewhere down the line
+console.log(Array.from(images)[0]);
 
 /*----- state variables -----*/
 
 let selectedImages = [0];
 let playerScore = 0;
+let remove = [0];
 
 /*----- cached elements  -----*/
-// const onClick = function (event) {
-//     let isFirstCardClicked = true;
-//     let convert = front.dataset.index
-//     console.log(convert)
-// }
 
 /*----- event listeners -----*/
 
 // need help with "else" condition
-handleClick()
+handleClick();
 
-function handleClick () {
+function handleClick() {
   images.forEach(function (image) {
     image.addEventListener("click", function () {
       let data = parseInt(image.dataset.index);
@@ -36,7 +31,10 @@ function handleClick () {
         console.log("winner!");
         selectedImages = [0];
         console.log(selectedImages);
-        playerScore += 1;
+        let clickedImage = event.target.className;
+        remove.push(clickedImage);
+        console.log(clickedImage);
+        console.log(remove);
       } else {
         if (selectedImages.length === 3) {
           console.log("wrong");
@@ -45,12 +43,47 @@ function handleClick () {
       }
       if (playerScore === 6) {
         console.log("YOU WIN");
-        scoreTracker.innerText = "Test"
+        scoreTracker.innerText = "Test";
+      }
+      // to remove images of what has matched thus far
+      if (remove[1] == "one") {
+        pictures.forEach(function (picture) {
+          picture.style.display = "none";
+        });
+        console.log("testing remove 2");
+        remove = [0];
       }
     });
   });
 }
 
+// btn.addEventListener("click", function () {
+//   images.forEach(function (image) {
+//     if (image.style.display === "flex") {
+//       image.style.display = "none"
+//     }
+//   })
+// })
+
+// function hide () {
+//   images.forEach(function (image) {
+//     if (image.style.display === "flex") {
+//       image.style.display = "none"
+//     }
+//   })
+// }
+
+// function toggle() {
+//   let x = document.getElementById("testhide");
+//   x.style.color = "red"
+//   console.log('test')
+
+// }
+
+// function toggle() {
+//   let fark = document.getElementsByClassName("front")
+//   fark.style.color = "red"
+// }
 
 // find a way to flip the images - transform / translate
 
