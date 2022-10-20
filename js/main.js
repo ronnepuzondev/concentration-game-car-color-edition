11034aaf36f7f6bc792f8be6f9c16f7d33a7adfa
rmap88 <ronnepdev@gmail.com>
2022-10-20 15:41:49 -0700

    add limit of 30 clicks

--- js/main.js
+++ js/main.js
@@ -1,46 +1,53 @@
 /*----- constants -----*/
 
+/*----- state variables -----*/
+
+let playerScore = 0;
+let clickCounter = 0;
+let selectedImages = [0];
+let hideAllTheImages = [];
+
+/*----- cached elements  -----*/
+
 const images = document.querySelectorAll(".front");
 const btn = document.querySelector("#playButton");
 const hiddenImages = document.querySelectorAll(".front img");
 const shuffleImages = document.querySelectorAll(".image");
-const howManyClicks = document.querySelector(".clicks span")
+const howManyClicks = document.querySelector(".clicks span");
+const gameOver = document.querySelector(".clicks");
 const scoreTracker = document.querySelector(".score");
-const winningFlag = document.querySelectorAll(".front")
-
-/*----- state variables -----*/
-
-let selectedImages = [0];
-let playerScore = 0;
-let hideAllTheImages = [];
-let clickCounter = 0
-
-/*----- cached elements  -----*/
 
 /*----- event listeners -----*/
 
-restartGame();
+shuffle();
 handleClick();
 
 function handleClick() {
   hiddenImages.forEach(function (image) {
     image.addEventListener("click", function () {
-      clickCounter += 1
-      howManyClicks.innerText = clickCounter
+      clickCounter += 1;
+      howManyClicks.innerText = clickCounter;
       hideAllTheImages.push(image);
       let data = parseInt(image.dataset.index);
       selectedImages.push(data);
       image.style.opacity = "1";
+      if (clickCounter === 30) {
+        scoreTracker.innerText = ".....GAME OVER.....";
+        scoreTracker.style.color = "red";
+        images.forEach(function (image) {
+          image.style.pointerEvents = "none";
+        });
+      }
       checkWinner();
     });
   });
-} 
+}
 
 /*----- functions -----*/
 
+// to check for win or lose conditions
 function checkWinner() {
   if (selectedImages[1] === selectedImages[2]) {
-    console.log("winner!");
     selectedImages = [0];
     hideAllTheImages = [];
     playerScore += 1;
@@ -50,56 +57,52 @@ function checkWinner() {
         hideAllTheImages[0].style.opacity = "0";
         hideAllTheImages[1].style.opacity = "0";
       }
-      setTimeout(hideTheImage, 1000);
+      setTimeout(hideTheImage, 800);
 
-      console.log("not the same image!");
       function clearSelectedImages() {
         selectedImages = [0];
         hideAllTheImages = [];
       }
-      //test from 1500 to 1100
-      setTimeout(clearSelectedImages, 1100);
-
+      setTimeout(clearSelectedImages, 900);
     }
   }
-
   if (playerScore === 6) {
-    console.log("YOU WIN");
     scoreTracker.innerText = "Winner Winner Chicken Dinner!";
     selectedImages = [0];
-    document.getElementById("container").style.backgroundImage = "url('css/images/fireworks.gif')"
-    hiddenImages.forEach(function(image) {
-      image.style.opacity = "0"
-    })
-    winningFlag.forEach(function(flag) {
-      flag.style.backgroundImage = "url('css/images/finishline.gif')"
-    })
-    
+    document.getElementById("container").style.backgroundImage =
+      "url('css/images/fireworks.gif')";
+    hiddenImages.forEach(function (image) {
+      image.style.opacity = "0";
+    });
+    images.forEach(function (flag) {
+      flag.style.backgroundImage = "url('css/images/finishline.gif')";
+    });
   }
 }
-
-
 // to reset game board and call shuffle function
 function restartGame() {
   hiddenImages.forEach(function (image) {
     image.style.opacity = "0";
     playerScore = 0;
     remove = [];
-    clickCounter = 0
+    clickCounter = 0;
     howManyClicks.innerText = 0;
-    document.getElementById("container").style.backgroundImage = ""
-    scoreTracker.innerText = "Click a race flag to reveal a car, remember its color and try to make your next race flag click match the previous car's color!"
-    shuffle()
-  })
-  winningFlag.forEach(function(flag) {
-    flag.style.backgroundImage = "url('css/images/raceflag.png')"
-  })
+    document.getElementById("container").style.backgroundImage = "";
+    scoreTracker.innerText =
+      "Click a race flag to reveal a car, remember its color and try to make your next race flag click match the previous car's color in under 30 CLICKS!";
+    scoreTracker.style.color = "white";
+  });
+  images.forEach(function (flag) {
+    flag.style.backgroundImage = "url('css/images/raceflag.png')";
+    flag.style.pointerEvents = "";
+  });
+  shuffle();
 }
 
-// shuffle function
+// shuffle images function
 function shuffle() {
-shuffleImages.forEach(function(card) {
-  let randomPos = Math.floor(Math.random() * 12);
-  card.style.order = randomPos;
-})
+  shuffleImages.forEach(function (card) {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
 }
